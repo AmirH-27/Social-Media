84a5b189abb49fdc7e798fd93fb091a0f5738ca3
AmirH-27 <habibamir92@gmail.com>
2022-09-22 13:15:24 +0600

    refactor spring security config file

--- src/main/java/com/SocialMedia/config/WebSecurityConfig.java
+++ src/main/java/com/SocialMedia/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.SocialMedia;
+package com.SocialMedia.config;
 import com.SocialMedia.service.CustomAuthProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
